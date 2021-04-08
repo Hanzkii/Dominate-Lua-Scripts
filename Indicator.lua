d0eec9400d5c4f32d6c74c68c496696586ea95cc
Hanzkii <55594145+Hanzkii@users.noreply.github.com>
2021-04-09 00:50:40 +0300

    Update Indicator.lua

--- Indicator.lua
+++ Indicator.lua
@@ -5,16 +5,14 @@ function OnPaint()
 	local TicksRecharged = GetRechargedTicks()
 	local PIMult = TicksRecharged / 16
 	local FinishRad = PIMult * math.pi * 2
+	local Height = height / 1.5
+	local Color = TicksRecharged * (255 / 16)
 
 	if DT:GetBool() == true and GetIsAlive(LocalPlayer) and TicksRecharged > 0 then --we alive and dt on
-		if TicksRecharged > 15 then --completed recharging
-			DrawText(50, height / 1.22, 50, 255, 50, 255, "Verdana", 24, "DT", true, true)
-		else
-			DrawText(50, height / 1.22, 50, 50, 50, 255, "Verdana", 24, "DT", true, true)
-		end
-		DrawFilledCircle(20,  height / 1.22, FinishRad * 2, 50, 255, 50, 255)
+		DrawText(50, Height, 50, Color, 50, 255, "Verdana", 24, "DT", true, true)
+		DrawFilledCircle(20,  Height, FinishRad * 2, 50, Color, 50, 255)
 	else
-		DrawText(50, height / 1.22, 50, 50, 50, 255, "Verdana", 24, "DT", true, true)
+		DrawText(50, Height, 50, 50, 50, 255, "Verdana", 24, "DT", true, true)
 	end
 end
 RegisterCallback("Paint", OnPaint)
