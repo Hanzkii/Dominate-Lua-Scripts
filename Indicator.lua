8f07f4f64f1d7c3161638de3a63ce7ee392ba88e
Hanzkii <55594145+Hanzkii@users.noreply.github.com>
2021-04-17 23:16:14 +0300

    Update Indicator.lua
    Fixed for new lua system

--- Indicator.lua
+++ Indicator.lua
@@ -1,18 +1,17 @@
 function OnPaint()
-	local DT = FindMenuControl("Rage_Global_DoubletapKeyBool")
-	local width, height = GetScreenSize()
-	local LocalPlayer = LocalPlayer()
-	local TicksRecharged = GetRechargedTicks()
-	local PIMult = TicksRecharged / 16
-	local FinishRad = PIMult * math.pi * 2
-	local Height = height / 1.5
-	local Color = TicksRecharged * (255 / 16)
+    local DT = FindMenuControl("Rage_Global_DoubletapKeyBool")
+    local width, height = GetScreenSize()
+    local LocalPlayer = LocalPlayer()
+    local TicksRecharged = GetRechargedTicks()
+    local CircleRad = 10 * (TicksRecharged / 16)
+    local Height = height / 1.5
+    local Color = TicksRecharged * (255 / 16)
 
-	if DT:GetBool() == true and GetIsAlive(LocalPlayer) and TicksRecharged > 0 then --we alive and dt on
-		DrawText(50, Height, 50, Color, 50, 255, "Verdana", 24, "DT", true, true)
-		DrawFilledCircle(20,  Height, FinishRad * 2, 50, Color, 50, 255)
-	else
-		DrawText(50, Height, 50, 50, 50, 255, "Verdana", 24, "DT", true, true)
-	end
+    if DT:GetBool() == true and GetIsAlive(LocalPlayer) and TicksRecharged > 0 then --we alive and dt on
+        DrawText(50, Height, 0, Color, 0, 255, "Verdana", 24, "DT", true, true)
+        DrawFilledCircle(20,  Height, math.floor(CircleRad), 0, Color, 0, 255)
+    else
+        DrawText(50, Height, 0, 0, 0, 255, "Verdana", 24, "DT", true, true)
+    end
 end
 RegisterCallback("Paint", OnPaint)
