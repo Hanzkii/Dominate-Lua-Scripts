a82036de1d7037165c16c8fbd10157dd091e9e63
Hanzkii <55594145+Hanzkii@users.noreply.github.com>
2021-04-19 16:17:24 +0300

    Update Indicator.lua
    update

--- Indicator.lua
+++ Indicator.lua
@@ -1,17 +1,38 @@
-function OnPaint()
-    local DT = FindMenuControl("Rage_Global_DoubletapKeyBool")
-    local width, height = GetScreenSize()
-    local LocalPlayer = LocalPlayer()
-    local TicksRecharged = GetRechargedTicks()
-    local CircleRad = 10 * (TicksRecharged / 16)
-    local Height = height / 1.5
-    local Color = TicksRecharged * (255 / 16)
 
-    if DT:GetBool() == true and GetIsAlive(LocalPlayer) and TicksRecharged > 0 then --we alive and dt on
-        DrawText(50, Height, 0, Color, 0, 255, "Verdana", 24, "DT", true, true)
-        DrawFilledCircle(20,  Height, math.floor(CircleRad), 0, Color, 0, 255)
-    else
-        DrawText(50, Height, 0, 0, 0, 255, "Verdana", 24, "DT", true, true)
-    end
+function OnPaint()
+	LoadFont("Tahoma")
+	local DT = FindMenuControl("Rage_Global_DoubletapKeyBool")
+	local OverrideDMG = FindMenuControl("Rage_Global_OverrideMindmgKeyBool")
+	local FD = FindMenuControl("AntiAim_Misc_FakeDuckBool")
+	local width, height = GetScreenSize()
+	local LocalPlayer = LocalPlayer()
+	local TicksRecharged = GetRechargedTicks()
+	local CircleRad = 10 * (TicksRecharged / 16)
+	local Color = TicksRecharged * (255 / 16)
+
+
+	if GetIsAlive(LocalPlayer) then
+
+		if FD:GetBool() == true then
+			DrawText(5, math.floor(height / 1.5), 0, Color, 0, 255, "Tahoma", 24, "FD", false, true)
+		else
+			DrawText(5, math.floor(height / 1.5), 0, 0, 0, 255, "Tahoma", 24, "FD", false, true)
+		end
+
+		if DT:GetBool() == true  and TicksRecharged > 0 and not FD:GetBool() == true then
+			DrawText(5,  math.floor(height / 1.55), 0, Color, 0, 255, "Tahoma", 24, "DT", false, true)
+			DrawFilledCircle(45,   math.floor(height / 1.55), math.floor(CircleRad), 0, Color, 0, 255)
+		else
+			DrawText(5,  math.floor(height / 1.55), 0, 0, 0, 255, "Tahoma", 24, "DT", false, true)
+		end
+
+		if OverrideDMG:GetBool() == true then
+			DrawText(5, math.floor(height / 1.60), 0, Color, 0, 255, "Tahoma", 24, "Damage", false, true)
+		else
+			DrawText(5, math.floor(height / 1.60), 0, 0, 0, 255, "Tahoma", 24, "Damage", false, true)
+		end
+
+	end
+
 end
 RegisterCallback("Paint", OnPaint)
