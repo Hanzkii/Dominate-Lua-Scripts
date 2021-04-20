f51932f173ea224f584f98b380221ecc5222a36d
Hanzkii <55594145+Hanzkii@users.noreply.github.com>
2021-04-20 14:49:40 +0300

    Update Indicator.lua

--- Indicator.lua
+++ Indicator.lua
@@ -8,26 +8,24 @@ function OnPaint()
 	local LocalPlayer = LocalPlayer()
 	local TicksRecharged = GetRechargedTicks()
 	local CircleRad = 10 * (TicksRecharged / 16)
-	local Color = TicksRecharged * (255 / 16)
-
 
 	if GetIsAlive(LocalPlayer) then
 
 		if FD:GetBool() == true then
-			DrawText(5, math.floor(height / 1.5), 0, Color, 0, 255, "Tahoma", 24, "FD", false, true)
+			DrawText(5, math.floor(height / 1.5), 0, 255, 0, 255, "Tahoma", 24, "FD", false, true)
 		else
 			DrawText(5, math.floor(height / 1.5), 0, 0, 0, 255, "Tahoma", 24, "FD", false, true)
 		end
 
 		if DT:GetBool() == true  and TicksRecharged > 0 and not FD:GetBool() == true then
-			DrawText(5,  math.floor(height / 1.55), 0, Color, 0, 255, "Tahoma", 24, "DT", false, true)
-			DrawFilledCircle(45,   math.floor(height / 1.55), math.floor(CircleRad), 0, Color, 0, 255)
+			DrawText(5,  math.floor(height / 1.55), 0, 255, 0, 255, "Tahoma", 24, "DT", false, true)
+			DrawFilledCircle(45,   math.floor(height / 1.55), math.floor(CircleRad), 0, 255, 0, 255)
 		else
 			DrawText(5,  math.floor(height / 1.55), 0, 0, 0, 255, "Tahoma", 24, "DT", false, true)
 		end
 
 		if OverrideDMG:GetBool() == true then
-			DrawText(5, math.floor(height / 1.60), 0, Color, 0, 255, "Tahoma", 24, "Damage", false, true)
+			DrawText(5, math.floor(height / 1.60), 0, 255, 0, 255, "Tahoma", 24, "Damage", false, true)
 		else
 			DrawText(5, math.floor(height / 1.60), 0, 0, 0, 255, "Tahoma", 24, "Damage", false, true)
 		end
